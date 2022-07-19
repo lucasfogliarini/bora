9d8edbe87f6446a8688bbe1ea4b5340f76c66d53
Lucas Fogliarini <lucas.fogliarini@way2.com.br>
2022-07-19 20:45:22 -0300

    add slider no create

--- src/app/models/event-create.model.ts
+++ src/app/models/event-create.model.ts
@@ -3,6 +3,7 @@ export class EventCreate {
   where?: string = 'Onde?';
   when?: string = 'Que dia?';
   quota?: string = 'Quanto vale esse encontro?';
+  currency?: string = 'R$';
   priceDefault: number = 50;
   titles = ['🥂 Camarote ou festa privada','💰 Empreender', '🔮 Criar', '🥩 Churrasco e bate-papo', '🏞 Viajar ou fazer uma trilha', '📱 Desenvolver um aplicativo' ];
   locations = ['🏡 Aqui em casa','🏛 Na Divagando','💻 Google Meet','⛩ Num Quiosque','🏖 Na Praia']
