a5e5804cf568b42dcdc7e5323cb8da254b404526
Lucas Fogliarini <lucas.fogliarini@way2.com.br>
2022-07-26 11:46:14 -0300

    ajuste EventCreate default currency and eval

--- src/app/models/event-create.model.ts
+++ src/app/models/event-create.model.ts
@@ -2,10 +2,10 @@ export class EventCreate {
   what?: string = 'O que vamos fazer?';
   where?: string = 'Onde?';
   when?: string = 'Quando?';
-  evaluation?: string = 'Quanto vale esse encontro?';
+  evaluation?: string = 'Quantas pessoas quer nesse encontro?';
   evaluationMax?: number = 30;
   evaluationDefault: number = 2;
-  currency?: string = '💃🕺';
+  currency?: string = '👥';
   startAtHour: number = 19;
   whatSuggestion: string = 'Tomar um café ...';
   startAt: Date = new Date(new Date().setHours(this.startAtHour,0,0,0));
