037700ef4cc5aaa99cdb51985db6d8b538a508a3
Lucas Fogliarini Pedroso <lucas.pedroso@cogna.com.br>
2023-07-15 23:16:18 -0300

    fix auth calendar button

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  year: number;
+  constructor(){
+    this.year = new Date().getFullYear();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   mainRole: 'Protagonista',
   secondRole: 'Espectador',
   appDefiniteArticle: 'o',
-  boraApi: 'https://api.vamo.fun/',
+  boraApi: 'https://bora.azurewebsites.net/',
   //boraApi: 'https://localhost:5001/',//must use /
 };
 
