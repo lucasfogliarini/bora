20cd404f65b54769b44c126a9c7c600a18184cf7
Lucas Fogliarini <lucasfogliarini@gmail.com>
2024-06-18 11:27:15 -0300

    Marcar uma consulta com o fundador

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,7 +7,11 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-  env = environment;
+  contactMessage = `Clique em <b>WhatsApp</b>, para: <br/>  
+- Marcar uma consulta com o <a href='/lucasfogliarini'>Fundador</a> <br/>
+- Ser uma(o) <b>${environment.mainRole}</b> <br/>
+- <b>Sugerir uma melhoria</b> <br/>
+- Relatar um <b>bug ou problema</b>`;
   open(){
     window.open(`https://api.whatsapp.com/send?phone=${environment.adminPhone}&text=Oi, quero conhecer ${environment.appDefiniteArticle} ${environment.appName}, ser ${environment.mainRole} ou relatar um problema ... `);
   }
