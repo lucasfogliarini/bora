a2c471759051d672c01b7468d049de9c6bae2780
Lucas Fogliarini <lucasfogliarini@gmail.com>
2024-06-19 11:13:06 -0300

    Ativar e Desativar Parceria

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -15,6 +15,24 @@ export class NavMenuComponent {
 
     this.authService.subscribeAuth();
   }
+  isPartner = this.authService.getAccount()?.isPartner;
+  togglePartnershipText = this.isPartner ? 'Desativar' : 'Ativar';
+  userType = this.isPartner ? 'Parceira(o)' : 'Usuário(o)';
+  partnership: string = `É muito bom ter você como <b>${this.userType}</b>!
+  <br/><br/>
+  <small>
+  A qualquer momento é possível <b>Ativar</b> e <b>Desativar</b> a <b>Parceria</b>.
+  <br />
+  <b>Enquanto ativado</b>:
+  <br/>
+  1. O botão <b>Bora?</b> é <b>exibido</b> no seu perfil para que <b>qualquer pessoa entre em contato pelo Google Agenda</b> <a href='https://api.bora.work/accounts/authorizeCalendar'>(caso sua agenda for autorizada)</a>
+  <br/>
+  2. Você é <b>convidado automaticamente<b/> nos encontros produzidos pelo <a href='/bora.work'><b>bora.work</b></a>
+  </small>
+  `;
+  togglePartnership(){
+
+  }
   getNowString(){
     const now = new Date();
     const date = now.toLocaleDateString();
