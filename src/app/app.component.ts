387abf0b6229f4b9d6ca5e427a4c4e5a5732c95c
Lucas Fogliarini <lucasfogliarini@gmail.com>
2024-07-04 14:27:22 -0300

    ajuste no meta e title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BoraApiService } from './bora-api.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,14 @@ import { BoraApiService } from './bora-api.service';
 export class AppComponent {
   version: string = "";
   constructor(
-    private boraApiService: BoraApiService) { }
+    private boraApiService: BoraApiService,
+    private titleService: Title) { }
   ngOnInit(): void {
     this.RequestVersion();
+    let title = "bora.work | Capacitando Engenheiros e Tecnologistas para as melhores soluções do mercado.";
+    if(window.location.hostname == "bora.social")
+      title = "bora.social | Produtora de eventos presenciais e virtuais em Porto Alegre e região";
+    this.titleService.setTitle(title);
   }
 
   isHome(){
